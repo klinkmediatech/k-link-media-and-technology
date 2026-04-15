08c6074fc118bf0851d4d9d6cef9e283c60a7c2e
klinkmediatech <okelloict@gmail.com>
2026-04-16 00:12:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzFUy1GlE3bl69co_7vG9iVTjOJZbkeNs",
+  authDomain: "k-link-media.firebaseapp.com",
+  projectId: "k-link-media",
+  storageBucket: "k-link-media.firebasestorage.app",
+  messagingSenderId: "911623086211",
+  appId: "1:911623086211:web:f67ab63e5d0bb0f55efbb6"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 function showMessage() {
   document.getElementById("msg").innerText =
     "✔ K-Link Media & Technology is active!";
